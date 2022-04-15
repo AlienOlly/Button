d5b33a2c4eccc4025b9c7734007f9f6a4888d697
AlienOlly <danmax32100@gmail.com>
2022-04-15 12:10:26 +0300

    button

---
+++ main.js
@@ -0,0 +1,59 @@
+const $score = document.querySelector('.score')
+const $container = document.querySelector('.container');
+const $limit = document.createElement('div');
+    $limit.classList.add('limit')
+const $btnBox = document.createElement('div')
+    $btnBox.classList.add('btnBox')
+    $btnBox.classList.add('limit')
+const $btn = document.createElement('a')
+    $btn.classList.add('btn')
+    $btn.innerText = 'Нажми'
+const $header = document.querySelector('.header')
+    const $phraseBg = document.createElement('div');
+    $phraseBg.classList.add('centering')
+const $phrase = document.createElement('p')
+    $phrase.classList.add('phrase')
+
+const WIDTH = 1500;
+const HEIGHT = 500;
+var count = 0;
+
+$btnBox.insertAdjacentElement('beforeend', $btn)
+$limit.insertAdjacentElement('beforeend', $btnBox)
+
+$limit.style.width = WIDTH + 'px';
+$limit.style.height = HEIGHT + 'px';
+
+$container.insertAdjacentElement('beforeend', $limit);
+$header.insertAdjacentElement('afterend', $phraseBg);
+$phraseBg.insertAdjacentElement('afterend', $phrase);
+
+var posLeft;
+var posTop;
+    
+function getRandomPos() {
+    posLeft = Math.floor(Math.random() * 1000);
+    posTop = Math.floor(Math.random() * 100);
+    
+    $btnBox.style.marginLeft = posLeft + 'px';
+    $btnBox.style.marginTop = posTop + 'px';
+    
+    $score.innerText = 'Раз промазал: ' + count;
+}
+
+function setRandomPhrases(){
+    const phrases = ['Мимо!', 'Промах!', 'Промахнулся!', 'Старайся лучше!', 'xD', 'Что, не нажимается?', 'Упс', 'Хоба!', 'Вжух!', ':('];
+    let n = Math.floor(Math.random() * 10);
+
+    return (phrases[n]);
+    // console.log(phrases[n]);
+}
+
+$btnBox.addEventListener('click', () => {
+    count++
+    $limit.classList.remove('limit')
+    $btnBox.classList.remove('limit');
+    $phrase.innerText = setRandomPhrases();
+
+    getRandomPos()
+})
